dbef19869ca515adae6f1b0f99e0b3fb51b9f236
Mai Mantun <maimantun@163.com>
2020-03-18 15:44:47 +0800

    增加git初始化配置脚本

---
+++ utils/gitconfig.sh
@@ -0,0 +1,5 @@
+
+git config --global core.editor /d/9--MyTools/notepad2-4.2.25/Notepad2.exe
+git config --global core.quotepath false
+git config --global user.email "maimantun@163.com"
+git config --global user.name "Mai Mantun"
